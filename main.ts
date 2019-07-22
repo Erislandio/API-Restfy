8cca7f3b875b02849eddad7994c68de3b7f21cda
Erislândio Soares <erislandio.soares@acct.global>
2019-07-21 23:07:23 -0300

    add methods and routes teste

--- main.ts
+++ main.ts
@@ -1,21 +1,39 @@
 import * as restify from "restify";
 import chalk from "chalk";
-
 const PORT = 3000;
 
-// criar server
+// * criar server
 const server = restify.createServer({
     name: "meat-api",
     version: "1.0.0"
 });
 
+// * criar rota
 server.get("/hello", (req, res, next) => {
+    res.setHeader("Content-type", "application/json");
+    res.status(200);
     res.json({
         message: "Hello"
     });
     return next();
 });
 
+// * middleware
+server.use(restify.plugins.queryParser());
+
+server.get("/info", (req, res, next) => {
+    res.json({
+        browser: req.userAgent(),
+        method: req.method,
+        url: req.url,
+        path: req.path(),
+        params: req.query
+    });
+
+    return next()
+});
+
+// * server listen
 server.listen(PORT, () => {
     console.log(
         chalk.blue(
