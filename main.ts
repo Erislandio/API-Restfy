f7f838d240d41f84a8bbb42646752f0804288927
Erislândio Soares <erislandio.soares@acct.global>
2019-07-21 22:55:35 -0300

    add color on terminal console

--- main.ts
+++ main.ts
@@ -1,7 +1,27 @@
 import * as restify from "restify";
+import chalk from "chalk";
+
+const PORT = 3000;
 
 // criar server
 const server = restify.createServer({
     name: "meat-api",
     version: "1.0.0"
+});
+
+server.get("/hello", (req, res, next) => {
+    res.json({
+        message: "Hello"
+    });
+    return next();
+});
+
+server.listen(PORT, () => {
+    console.log(
+        chalk.blue(
+            `Server is running on ${chalk.bgGreen.white.bold(
+                `http://localhost:${PORT}`
+            )}`
+        )
+    );
 });
