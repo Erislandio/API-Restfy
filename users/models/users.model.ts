c00dfd37bd29df8ce2d7a443681cc78dbd88f7e2
Erislandio <erislandiosoares21@gmail.com>
2019-07-23 23:14:44 -0300

    add route user by id

--- users/models/users.model.ts
+++ users/models/users.model.ts
@@ -1,16 +1,29 @@
 const users = [
     {
+        id: '1',
         name: "João",
         email: "joao123@gmail.com"
     },
     {
-        name: "João",
-        email: "joao123@gmail.com"
+        id: '2',
+        name: "Erislandio",
+        email: "erislandio.soares@gmail.com"
     }
 ]
 
 export class User {
     static findAll(): Promise<any[]> {
         return Promise.resolve(users)
+    }
+
+    static findById(id: string): Promise<any>{
+        return new Promise(resolve => {
+            const filtered = users.filter(user => user.id == id)
+            let user = undefined;
+            if(filtered.length){
+                user = filtered[0]
+            }
+            resolve(user)
+        })
     }
 }
--- users/user.router.ts
+++ users/user.router.ts
@@ -5,11 +5,24 @@ import { User } from "./models/users.model";
 class UsersRouter extends Router {
     applyRouters(application: restify.Server) {
         application.get("users", (req, res, next) => {
-           User.findAll().then(users => {
-               res.json(users)
-               return next
-           })
+            User.findAll().then(users => {
+                res.json(users)
+                return next()
+            })
         });
+
+        application.get('users/:id', (req, res, next) => {
+            User.findById(req.params.id).then(user => {
+                if (user) {
+                    res.json(user)
+                    return next()
+                }
+
+                res.send(404)
+                return next()
+
+            })
+        })
     }
 }
 
